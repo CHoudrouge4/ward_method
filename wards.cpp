28904c21eeb2659b33860c6a1826256c16dbc890
CHoudrouge4 <hah51@mail.aub.edu>
2019-04-01 19:36:20 +0200

    init

--- wards.cpp
+++ wards.cpp
@@ -1,1 +1,36 @@
 #include "wards.h"
+#include <fstream>
+#include <cmath>
+#define x(i) 2 * i
+#define y(i) 2 * i + 1
+
+double wards::distance(uint i, uint j) {
+  double dx = pts[x(i)] - pts[x(j)];
+  double dy = pts[y(i)] - pts[y(j)];
+  double d2 = dx * dx + dy * dy;
+  return sqrt(d2);
+}
+
+wards::wards(const std::string file_name) {
+  read_file(file_name);
+}
+
+void wards::read_file(const std::string file_name) {
+  std::ifstream in(file_name);
+  int number_points;
+  in >> number_points >> dimension;
+  pts = point(number_points * dimension);
+  for(int i = 0; i < dimension * number_points; ++i) in >> pts[i];
+  in.close();
+}
+
+void wards::clusster() {
+  uint n = pts.size() / dimension;
+  M = std::vector<point>(n, std::vector<double>(n));
+  for(uint i = 0; i < n; ++i) {
+    M[i][i] = 0;
+    for(uint j = i + 1; j < n; ++j) {
+      M[i][j] = M[j][i] = distance(i, j);
+    }
+  }
+}
--- wards.h
+++ wards.h
@@ -3,14 +3,18 @@
 #define x(i) 2 * i
 #define y(i) 2 * i + 1
 
+typedef std::vector<double> point;
+typedef unsigned int uint;
+
 class wards {
 private:
   std::vector<double> pts;
+  std::vector<point> M; // dissimilarity matrix,
   int dimension;
 
   void read_file(const std::string);
+  double distance(uint i, uint j);
   void clusster();
 public:
-  wards(const std::string, int);
-
+  wards(const std::string);
 };
