1754aa6e37516727166382f9bbe8d4ca650bd78e
CHoudrouge4 <hah51@mail.aub.edu>
2019-05-08 11:16:36 +0200

    c++ bug does not exist

--- cpp/main.cc
+++ cpp/main.cc
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <vector>
+#include <random>
 
 float * read_file(const std::string file_name, int &n, int &m) {
   std::ifstream in(file_name);
@@ -18,39 +19,94 @@ float * read_file(const std::string file_name, int &n, int &m) {
   return array;
 }
 
-int main () {
+float * generate_random_matrix(int n, int m) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(1.0, 5.0);
+  float * array = (float *) malloc(n * m * sizeof(float));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      * (array + i * m + j) = dis(gen);
+    }
+  }
+  return array;
+}
 
-  int n;
-  int d;
-  float * result = read_file("data.in", n, d);
-  flann::Matrix<float> dataset(result, n, d);
+void printing_result (const std::vector<std::vector<int>> &indices, const std::vector<std::vector<float>> &dists) {
+  std::cout << "Indices" << '\n';
+  for (auto e: indices) {
+      for (auto ee: e) std::cout << ee << '\n';
+  }
+  std::cout << "Distance" << '\n';
+  for (auto d_: dists) {
+      for (auto d: d_) std::cout << d << '\n';
+  }
+}
+
+void print_matrix (flann::Matrix<float> &dataset, int n, int d) {
   for (int i = 0; i < n; ++i) {
      for (int j = 0; j < d; ++j) {
        std::cout << dataset[i][j] << ' ';
      }
      std::cout << '\n';
   }
+}
+
+int main () {
+
+  int n = 5;
+  int d = 2;
+  float * result = generate_random_matrix(n, d);
+
+  flann::Matrix<float> dataset(result, n, d);
+  print_matrix(dataset, n, d);
 
   flann::Index<flann::L2<float>> index(dataset, flann::KDTreeIndexParams(4));
   index.buildIndex();
 
   std::vector<float> pt_(d);
   for(int i = 0; i < d; ++i)  {
-     pt_[i] = 1.0;
+     pt_[i] = dataset[0][i];
      std::cout << "1d : " << pt_[i] << '\n';
   }
 
-  flann::Matrix<float> pt(pt_.data(), 1, d);
-  index.addPoints(pt);
+  flann::Matrix<float> query(pt_.data(), 1, d);
+  std::vector<std::vector<int>> indices;
+  std::vector<std::vector<float>> dists;
 
-  for (int j = 0; j < d; ++j) {
-    std::cout << pt[0][j] << ' ';
-  }
-  std::cout << '\n';
+  index.knnSearch(query, indices, dists, 1,  flann::SearchParams(128));
 
+  printing_result(indices, dists);
 
+  index.removePoint(indices[0][0]);
+
+  print_matrix(dataset, n, d);
+
+  indices.clear();
+  dists.clear();
+
+  index.knnSearch(query, indices, dists, 1,  flann::SearchParams(128));
+  printing_result(indices, dists);
+
+  index.addPoints(query);
+
+  index.knnSearch(query, indices, dists, 1,  flann::SearchParams(128));
+  printing_result(indices, dists);
+
+  index.removePoint(indices[0][0]);
+
+  index.knnSearch(query, indices, dists, 1,  flann::SearchParams(128));
+  printing_result(indices, dists);
+  // index.addPoints(pt);
+  //
+  // for (int j = 0; j < d; ++j) {
+  //   std::cout << pt[0][j] << ' ';
+  // }
+  // std::cout << '\n';
+  //
+  //
 
   delete [] dataset.ptr();
-  delete [] pt.ptr();
+  delete [] query.ptr();
   return 0;
 }
