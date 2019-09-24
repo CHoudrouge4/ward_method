a64b1bb5d18061de9c192e18a309a2087ea6fabd
CHoudrouge4 <hah51@mail.aub.edu>
2019-09-24 15:40:40 +0200

    write python exp

--- read_tree.py
+++ read_tree.py
@@ -2,6 +2,29 @@ from sklearn import datasets
 from sklearn.cluster import AgglomerativeClustering
 from numpy import *
 import time
+from numpy import *
+
+import sys
+sys.setrecursionlimit(12000)
+
+def get_news_group(dimension):
+    from sklearn.datasets import fetch_20newsgroups
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.decomposition import PCA
+
+    newsgroups_train = fetch_20newsgroups(subset='train')
+    vectorizer = TfidfVectorizer(min_df=0.01, max_df=0.95)
+    train_data = vectorizer.fit_transform(newsgroups_train.data)
+    train_data = train_data.todense()
+    train_labels = newsgroups_train.target;
+
+    # initialise PCA with n_components = dimension
+    pca = PCA(n_components=dimension)
+    # apply pca
+    pca.fit(train_data)
+    Y = pca.transform(train_data)
+    n_elements = len(unique(train_labels))
+    return Y, n_elements, train_labels, len(set(train_labels))
 
 def read_file(filename):
     f = open(filename, "r")
@@ -36,10 +59,7 @@ def read_file(filename):
 def get_cluster(tree, index):
     if tree[index][0] == index:
         return [index]
-
-    return get_cluster(tree,
-                       tree[index][0])+get_cluster(tree,
-                                                   tree[index][1])
+    return get_cluster(tree, tree[index][0]) + get_cluster(tree, tree[index][1])
 
 def clusters(tree, k):
     nums = []
@@ -93,7 +113,7 @@ def get_dataset(name):
 # t = [[0, -2], [1, -1], [2, -1], [3, -1], [4, -1], [0, 1], [2, 3], [5, 6], [7, 4]]
 
 # print(clusters(t, 3))
-from sklearn.datasets import load_iris
+#from sklearn.datasets import load_iris
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
 def convert(clusters, n):
@@ -104,25 +124,25 @@ def convert(clusters, n):
     return clustering_vect
 
 
-#epsilon = 200
-# numebr_of_trees = 16
-# number_of_visited_leafs = 128
-# #print("epsilon ", "0." + str(epsilon), numebr_of_trees, number_of_visited_leafs)
-#
+def approx_vs_ward(e, number_of_visited_leafs, numebr_of_trees, name, dimension):
+
+#print("epsilon ", "0." + str(epsilon), numebr_of_trees, number_of_visited_leafs)
 # #eps = [25, 50, 75, 85, 95, 200, 400];
-# eps = [400]
-# data_sets = ["iris", "cancer", "digits", "boston"]
-# #data_sets = ["digits"]
-# for e in eps:
-#     output_file = 'result' + str(e) + '_' + str(numebr_of_trees) + '_' + str(number_of_visited_leafs) + '.txt'
-#     with open(output_file, 'w') as file:
-#         file.write('epsilon 0.' + str(e) + ' ' + str(numebr_of_trees) + ' ' + str(number_of_visited_leafs) + '\n')
-#         for name in data_sets:
-#             data, n, labels, k = get_dataset(name)
-#             file_name = './data/' + name + str(e) + '_' + str(numebr_of_trees) + '_' + str(number_of_visited_leafs) + ".out"
-#             T = read_file(file_name)
-#             clust = clusters(T, k)
-#             file.write('Algo ' + str(normalized_mutual_info_score(convert(clust, len(labels)), labels)) + '\n')
+
+    output_file = 'result' + str(e) + '_' + str(numebr_of_trees) + '_' + str(number_of_visited_leafs) + '.txt'
+    with open(output_file, 'w') as file:
+        file.write('epsilon 0.' + str(e) + ' ' + str(numebr_of_trees) + ' ' + str(number_of_visited_leafs) + '\n')
+        data, n, labels, k = get_news_group(dimension)
+        file_name = './' + name + '_' + str(dimension) + '_' + str(e) + '_' + str(numebr_of_trees) + '_' + str(number_of_visited_leafs) + ".out"
+        T = read_file(file_name)
+        print (k)
+        clust = clusters(T, k)
+        file.write('Algo ' + str(normalized_mutual_info_score(convert(clust, len(labels)), labels)) + '\n')
+        ward = AgglomerativeClustering(n_clusters=k, linkage='ward', connectivity=None)
+        clustering = ward.fit(data)
+        clust = clustering.labels_
+        file.write('std_ward ' + str(normalized_mutual_info_score(clust, labels)) + '\n')
+
 #
 #         for name in data_sets:
 #             data, n, labels, k = get_dataset(name)
@@ -169,12 +189,12 @@ def readFILE(file_name):
   # std::vector<int> leaves = {5, 128};
   # std::vector<float> epsilons = {0.5, 1, 7};
 
-trees = [2]
-leaves = [10]
-epsilons = [800]
-d = 20
-k = 10
-ward = AgglomerativeClustering(n_clusters=k, linkage='ward', connectivity=None)
+#trees = [2]
+#leaves = [10]
+#epsilons = [800]
+#d = 20
+#k = 10
+#ward = AgglomerativeClustering(n_clusters=k, linkage='ward', connectivity=None)
 # with open('ward_accuracy7.txt', 'w') as f:
 #     for e in epsilons:
 #         for t in trees:
@@ -207,5 +227,7 @@ ward = AgglomerativeClustering(n_clusters=k, linkage='ward', connectivity=None)
 # T = read_file(file_name)
 # clust = clusters(T, k)
 # print(normalized_mutual_info_score(convert(clust, len(labels)), labels))
-data, n, labels, k = get_dataset('boston')
-print(k)
+#data, n, labels, k = get_dataset('boston')
+#print(k)
+## e psilon, number_of_visited_leafs, number_of_trees, dimension
+approx_vs_ward(800, 10, 2, 'news11314', 100)
